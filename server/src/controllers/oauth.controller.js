b1115c84b1338fa7b622e0df15b04276da6a26d8
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 18:01:53 +0300

    Add new startGoogleOAuth function to oauth controller

--- server/src/controllers/oauth.controller.js
+++ server/src/controllers/oauth.controller.js
@@ -1,10 +1,36 @@
 import { env } from '../config/env.js';
-import { accessTokenCookieOptions, refreshTokenCookieOptions } from '../config/cookies.js';
+
+import {
+  accessTokenCookieOptions,
+  oauthStateCookieOptions,
+  refreshTokenCookieOptions,
+} from '../config/cookies.js';
+
 import { AppError } from '../errors/AppError.js';
+
 import { createAuthentication } from '../services/auth.service.js';
+
+import { generateOAuthState, verifyOAuthState } from '../services/oauth.state.service.js';
+
+export function startGoogleOAuth(req, res) {
+  const state = generateOAuthState();
+
+  res.cookie('oauthState', state, oauthStateCookieOptions);
+
+  return res.redirect('/auth/google/authorize');
+}
 
 export async function googleCallback(req, res, next) {
   try {
+    const receivedState = req.query.state;
+    const expectedState = req.cookies.oauthState;
+
+    if (!verifyOAuthState(expectedState, receivedState)) {
+      throw new AppError('OAuth authentication failed.', 401, 'OAUTH_STATE_INVALID');
+    }
+
+    res.clearCookie('oauthState', oauthStateCookieOptions);
+
     if (!req.user) {
       throw new AppError('OAuth authentication failed.', 401, 'OAUTH_AUTHENTICATION_FAILED');
     }
