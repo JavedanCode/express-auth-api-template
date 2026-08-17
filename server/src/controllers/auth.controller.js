df0526b75c4e150ad4cbec647254f036acdade15
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 14:21:47 +0300

    Add register function to auth controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,31 @@
-import { createAuthentication } from '../services/auth.service.js';
+import { createAuthentication, registerUser } from '../services/auth.service.js';
 import { env } from '../config/env.js';
+
+export async function register(req, res, next) {
+  try {
+    const { username, email, password } = req.body;
+
+    const user = await registerUser({
+      username,
+      email,
+      password,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: 'Registration successful.',
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        displayName: user.displayName,
+        avatarUrl: user.avatarUrl,
+      },
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
 
 export async function login(req, res, next) {
   try {
