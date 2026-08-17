5de1dad8c2abde6697841efca8cdfbec30cb3c4b
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 17:15:59 +0300

    Add rate limiter to routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -5,18 +5,23 @@ import { getMe, login, refresh, register, logout } from '../controllers/auth.con
 import { authenticateLocal } from '../middleware/passport.js';
 import { validate } from '../middleware/validate.js';
 import { authenticate } from '../middleware/authenticate.js';
+import {
+  loginRateLimiter,
+  refreshRateLimiter,
+  registerRateLimiter,
+} from '../middleware/rate-limit.middleware.js';
 
 import { loginSchema, registerSchema } from '../schemas/auth.schema.js';
 
 const router = Router();
 
-router.post('/register', validate(registerSchema), register);
+router.post('/register', registerRateLimiter, validate(registerSchema), register);
 
-router.post('/login', validate(loginSchema), authenticateLocal, login);
+router.post('/login', loginRateLimiter, validate(loginSchema), authenticateLocal, login);
 
 router.get('/me', authenticate, getMe);
 
-router.post('/refresh', refresh);
+router.post('/refresh', refreshRateLimiter, refresh);
 
 router.post('/logout', logout);
 
