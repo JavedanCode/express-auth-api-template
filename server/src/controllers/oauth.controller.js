edbd748b2170dd190d3b768c21ed585f0309eb83
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 17:24:03 +0300

    Add oauth controller

---
+++ server/src/controllers/oauth.controller.js
@@ -0,0 +1,33 @@
+import { env } from '../config/env.js';
+
+import { accessTokenCookieOptions, refreshTokenCookieOptions } from '../config/cookies.js';
+
+import { AppError } from '../errors/AppError.js';
+
+import { createAuthentication } from '../services/auth.service.js';
+
+export function googleLogin(req, res, next) {
+  return next();
+}
+
+export async function googleCallback(req, res, next) {
+  try {
+    if (!req.user) {
+      throw new AppError('OAuth authentication failed.', 401, 'OAUTH_AUTHENTICATION_FAILED');
+    }
+
+    const { accessToken, refreshToken } = await createAuthentication({
+      userId: req.user.id,
+      userAgent: req.get('user-agent'),
+      ipAddress: req.ip,
+    });
+
+    res.cookie('accessToken', accessToken, accessTokenCookieOptions);
+
+    res.cookie('refreshToken', refreshToken, refreshTokenCookieOptions);
+
+    return res.redirect(env.CLIENT_URL);
+  } catch (error) {
+    return next(error);
+  }
+}
