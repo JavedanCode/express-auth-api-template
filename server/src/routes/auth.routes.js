afbb81d081e6098cde59bfdf21c3a64a22147e57
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 16:04:32 +0300

    Add get me route to auth routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
-import { login, register } from '../controllers/auth.controller.js';
+import { login, register, getMe } from '../controllers/auth.controller.js';
 
 import { authenticateLocal } from '../middleware/passport.js';
 import { validate } from '../middleware/validate.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 import { loginSchema, registerSchema } from '../schemas/auth.schema.js';
 
@@ -12,5 +13,7 @@ const router = Router();
 router.post('/register', validate(registerSchema), register);
 
 router.post('/login', validate(loginSchema), authenticateLocal, login);
+
+router.get('/me', authenticate, getMe);
 
 export default router;
