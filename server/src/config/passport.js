7638f0f42a2ee6018c7d4dad713a1e717fb48f2d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-17 17:23:21 +0300

    Add google strategy to passport config

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -1,9 +1,11 @@
 import passport from 'passport';
 
 import { configureLocalStrategy } from '../strategies/local.strategy.js';
+import { configureGoogleStrategy } from '../strategies/google.strategy.js';
 
 export function configurePassport() {
   configureLocalStrategy();
+  configureGoogleStrategy();
 
   return passport;
 }
