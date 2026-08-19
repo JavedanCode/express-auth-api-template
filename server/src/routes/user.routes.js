87916d9b071701fb3884af5e6dcf391cb0f4e36d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-19 10:12:28 +0300

    Add profile update route /me to user routes

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
-import { changePassword } from '../controllers/user.controller.js';
+import { changePassword, updateProfile } from '../controllers/user.controller.js';
+
 import { authenticate } from '../middleware/authenticate.js';
+
 import { validate } from '../middleware/validate.js';
+
 import { changePasswordSchema } from '../schemas/auth.schema.js';
 
+import { updateProfileSchema } from '../schemas/user.schema.js';
+
 const router = Router();
+
+router.patch('/me', authenticate, validate(updateProfileSchema), updateProfile);
 
 router.patch('/me/password', authenticate, validate(changePasswordSchema), changePassword);
 
