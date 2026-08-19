e963f53c9f6a56be36ddb7d544ed13ec5085eaad
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-19 11:05:24 +0300

    Add email change controllers to user controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import { changeUserPassword, updateUserProfile, changeUsername } from '../services/user.service.js';
-
+import { requestEmailChange, confirmEmailChange } from '../services/email-change.service.js';
 export async function updateProfile(req, res, next) {
   try {
     const { displayName, avatarUrl } = req.body;
@@ -55,6 +55,36 @@ export async function changeUsernameController(req, res, next) {
         id: user.id,
         username: user.username,
       },
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function requestEmailChangeController(req, res, next) {
+  try {
+    const { email } = req.body;
+
+    await requestEmailChange(req.user.id, email);
+
+    return res.status(200).json({
+      success: true,
+      message: 'If the email can be changed, a verification email will be sent.',
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function confirmEmailChangeController(req, res, next) {
+  try {
+    const { token } = req.body;
+
+    await confirmEmailChange(req.user.id, token);
+
+    return res.status(200).json({
+      success: true,
+      message: 'Email address changed successfully.',
     });
   } catch (error) {
     return next(error);
