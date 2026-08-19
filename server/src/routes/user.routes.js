43f1cc958f7de43b818cc55fb8462c460c08e78d
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-19 10:22:25 +0300

    Add username change route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,6 +1,10 @@
 import { Router } from 'express';
 
-import { changePassword, updateProfile } from '../controllers/user.controller.js';
+import {
+  changePassword,
+  updateProfile,
+  changeUsernameController,
+} from '../controllers/user.controller.js';
 
 import { authenticate } from '../middleware/authenticate.js';
 
@@ -8,12 +12,19 @@ import { validate } from '../middleware/validate.js';
 
 import { changePasswordSchema } from '../schemas/auth.schema.js';
 
-import { updateProfileSchema } from '../schemas/user.schema.js';
+import { updateProfileSchema, updateUsernameSchema } from '../schemas/user.schema.js';
 
 const router = Router();
 
 router.patch('/me', authenticate, validate(updateProfileSchema), updateProfile);
 
 router.patch('/me/password', authenticate, validate(changePasswordSchema), changePassword);
+
+router.patch(
+  '/me/username',
+  authenticate,
+  validate(updateUsernameSchema),
+  changeUsernameController,
+);
 
 export default router;
