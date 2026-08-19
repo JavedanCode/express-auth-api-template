c21ff75ffcf4cbcd545ecab9ec9ace669809dcca
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-19 10:22:11 +0300

    Add username change controller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import { changeUserPassword, updateUserProfile } from '../services/user.service.js';
+import { changeUserPassword, updateUserProfile, changeUsername } from '../services/user.service.js';
 
 export async function updateProfile(req, res, next) {
   try {
@@ -33,6 +33,28 @@ export async function changePassword(req, res, next) {
     return res.status(200).json({
       success: true,
       message: 'Password changed successfully. Please log in again.',
+    });
+  } catch (error) {
+    return next(error);
+  }
+}
+
+export async function changeUsernameController(req, res, next) {
+  try {
+    const { username } = req.body;
+
+    const user = await changeUsername({
+      userId: req.user.id,
+      username,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: 'Username updated successfully.',
+      user: {
+        id: user.id,
+        username: user.username,
+      },
     });
   } catch (error) {
     return next(error);
