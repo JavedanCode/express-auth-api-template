c8f63f14a03de98476c79d84a01099a8aa87a1da
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-20 15:44:32 +0300

    Change invalid verifyPassword import to the correct path

--- server/src/strategies/local.strategy.js
+++ server/src/strategies/local.strategy.js
@@ -1,7 +1,7 @@
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 
-import { verifyPassword } from '../services/auth.service.js';
+import { verifyPassword } from '../services/password.service.js';
 import { findUserByEmail } from '../services/user.service.js';
 
 export function configureLocalStrategy() {
