b6eb0929f431538f0c4a50fa52939bb61454af6e
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-20 15:38:47 +0300

    Move passwordSchema into common.schema and import it from there on other schemas and files that use it

--- server/src/schemas/auth.schema.js
+++ server/src/schemas/auth.schema.js
@@ -1,9 +1,5 @@
 import { z } from 'zod';
-
-const passwordSchema = z
-  .string()
-  .min(8, 'Password must be at least 8 characters long.')
-  .max(128, 'Password must not exceed 128 characters.');
+import { passwordSchema } from './common.schema';
 
 export const registerSchema = z.object({
   username: z
--- server/src/schemas/user.schema.js
+++ server/src/schemas/user.schema.js
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+import { passwordSchema } from './common.schema';
+
 export const updateProfileSchema = z
   .object({
     displayName: z
