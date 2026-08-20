1773369041ffe8e0323e1939d48675bd9f24d397
Mohammadmahdi Mohammadinia <sorenjavedan@gmail.com>
2026-08-20 17:57:20 +0300

    Normalize email service errors

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,6 +2,8 @@ import { Resend } from 'resend';
 
 import { env } from '../config/env.js';
 
+import { AppError } from '../errors/AppError.js';
+
 const resend = new Resend(env.RESEND_API_KEY);
 
 export async function sendEmail({ to, subject, html }) {
@@ -13,7 +15,11 @@ export async function sendEmail({ to, subject, html }) {
   });
 
   if (error) {
-    throw new Error(`Failed to send email: ${error.message}`);
+    throw new AppError(
+      'Unable to deliver the email. Please try again later.',
+      503,
+      'EMAIL_DELIVERY_FAILED',
+    );
   }
 
   return data;
